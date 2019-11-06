c8dea8e027bf85f3a6d277eb343d3b568b409235
Dnpypy <denlatypoff@yandex.ru>
2019-11-07 02:57:41 +0300

    add method init_db, edit readme.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-sinatra-bootstrap-clean
-=======================
+Leprosorium.
 
-Cleaned version of https://github.com/bootstrap-ruby/sinatra-bootstrap
+this folder github rubyschool-us: git clone https://github.com/rubyschool-us/sinatra-bootstrap-clean.git
+
+edit GEMFILE                     # gem 'sinatra-contrib' # To work reloader
+
+edit GEMFILE                     # gem 'sqlite3'         # To work sqlite db
+
+bundle install                   # To install sinatra bundle
+
+bundle exec ruby app.rb    # To run the sample cmd window
--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+=begin
+Чтобы глобальную переменную
+можно было использовать во всех запросах
+используем метод before(выполняется перед выполнением любого запроса)
+=end
+
+# метод init_db буду использовать и в before и в configure
+def init_db
+	# Библиотека SQLite3:: Класс Database
+	# . метод new "принимает параметр имя нашей базы данных"
+	@db = SQLite3::Database.new "leprosorium.db"
+	# Результаты возвращаются в виде хэша, а не массива
+	@db.results_as_hash = true
+end
+
+before do
+
+end
 
 get '/' do
 	@title = "Leprosorium"
